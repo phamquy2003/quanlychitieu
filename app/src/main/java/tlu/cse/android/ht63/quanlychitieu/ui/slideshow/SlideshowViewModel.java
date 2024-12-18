ad09bf83a4bb38e71e16ca8014eb1928207b690d
phamquy2003 <anhquyk797@gmail.com>
2024-12-18 13:11:14 +0700

    Initial commit

---
+++ app/src/main/java/tlu/cse/android/ht63/quanlychitieu/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package tlu.cse.android.ht63.quanlychitieu.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
